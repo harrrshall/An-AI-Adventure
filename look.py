f36319ed0affa4e58a06beb6260503c6733b758c
CyberNovas <92268499+harrrshall@users.noreply.github.com>
2023-12-16 20:46:56 +0530

    Update look.py

--- look.py
+++ look.py
@@ -3,11 +3,7 @@ import time
 from PIL import Image
 import numpy as np
 import os
-import cv2
-import time
-from PIL import Image
-import numpy as np
-import os
+
 # Folder
 output_folder = "captured_frames"
 
